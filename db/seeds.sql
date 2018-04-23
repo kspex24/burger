2e2c17bf89b19c4eb4cf0ca591cde4c3427ca4fa
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-04-23 09:59:22 -0400

    most functionality working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheddar BBQ");
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Swiss");
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");source schema.sql`
